602d7110a22cf2d33983ad3c23c9fef4af09c5cc
2Erik <erik19yogo@gmail.com>
2026-05-17 07:22:26 -0500

    ERT: Taller finalizado

---
+++ src/main/java/com/krakeDev/Taller/Exepciones/Test.java
@@ -0,0 +1,24 @@
+package com.krakeDev.Taller.Exepciones;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(GuardarContacto.class);
+
+	public static void main(String[] args) {
+		
+		try {
+			ValidarContacto.validarTelefono("123");
+			GuardarContacto.main(args);
+			LeerContacto.main(args);
+			LOGGER.info("Contacto guardado");
+		} catch (Exception e) {
+			
+			LOGGER.error("Contacto no valido: ",e.getMessage());
+		}
+
+	}
+
+}

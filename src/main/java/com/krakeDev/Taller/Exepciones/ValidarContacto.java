4fd84bcf9bd6a3873e573ab9208d7c112a280dd6
2Erik <erik19yogo@gmail.com>
2026-05-21 16:29:00 -0500

    ERT: Corregido parte 3 IllegaalArgumentException

--- src/main/java/com/krakeDev/Taller/Exepciones/ValidarContacto.java
+++ src/main/java/com/krakeDev/Taller/Exepciones/ValidarContacto.java
@@ -2,9 +2,9 @@ package com.krakeDev.Taller.Exepciones;
 
 public class ValidarContacto {
 	
-	public static void validarTelefono(String telefono)throws Exception {
+	public static void validarTelefono(String telefono)throws IllegalArgumentException {
 		if(telefono.length() != 10) {
-			throw new Exception("No tiene 10 digitos");
+			throw new IllegalArgumentException("No tiene 10 digitos");
 		}
 	}
 }
